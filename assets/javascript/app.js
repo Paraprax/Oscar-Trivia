c8d37dfc6fc0b5acd4e1fcc65c486b785a368696
Paraprax <parkerjohnston28@gmail.com>
2018-10-24 15:06:25 -0400

    centered timer-box column finally

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,55 @@
 console.log("Hello, Newman")
 
 $(document).ready(function(){
+
+    console.log("Hello, Newman")
+
+    function getAnswer(){
+        // Get the selected score (assuming one was selected)
+        var answer = document.querySelector('input[name="scores"]:checked').value;
+        alert(score + ' was selected!');
+    }
    
+    function buildQuiz(questions, quizContainer, resultsContainer, submitButton){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        function showQuestions(questions, quizContainer){
+            // code will go here
+        }
     
+        function showResults(questions, quizContainer, resultsContainer){
+            // code will go here
+        }
+    
+        // show the questions
+        showQuestions(questions, quizContainer);
+    
+        // when user clicks submit, show results
+        $('#results').on("click", function() {
+            showResults(questions, quizContainer, resultsContainer);
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
